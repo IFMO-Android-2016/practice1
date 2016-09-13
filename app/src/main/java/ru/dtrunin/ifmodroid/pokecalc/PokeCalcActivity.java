142b9fc26f7263fc26c942d315f551ea08e70ddb
Trunin Dmitry <dmitry.trunin@corp.mail.ru>
2016-09-13 17:50:52 +0300

    Handle pokemon selection.

--- app/src/main/java/ru/dtrunin/ifmodroid/pokecalc/PokeCalcActivity.java
+++ app/src/main/java/ru/dtrunin/ifmodroid/pokecalc/PokeCalcActivity.java
@@ -1,14 +1,17 @@
 package ru.dtrunin.ifmodroid.pokecalc;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.Spinner;
 
-public class PokeCalcActivity extends AppCompatActivity {
+public class PokeCalcActivity extends AppCompatActivity implements Spinner.OnItemSelectedListener {
 
     ImageView pokemonImageView;
     Spinner pokemonSpinnerView;
@@ -17,6 +20,8 @@ public class PokeCalcActivity extends AppCompatActivity {
     EditText hpView;
     Button doCalcButton;
     ScrollView scrollView;
+
+    TypedArray pokemonImageIds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +35,21 @@ public class PokeCalcActivity extends AppCompatActivity {
         hpView = (EditText) findViewById(R.id.hp_text);
         doCalcButton = (Button) findViewById(R.id.do_calc_btn);
         scrollView = (ScrollView) findViewById(R.id.scrol_view);
+
+        pokemonSpinnerView.setOnItemSelectedListener(this);
+
+        pokemonImageIds = getResources().obtainTypedArray(R.array.pokemon_images);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+        final int imageId = pokemonImageIds.getResourceId(pos, 0);
+        pokemonImageView.setImageResource(imageId);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        pokemonImageView.setImageResource(0);
     }
 
 }
